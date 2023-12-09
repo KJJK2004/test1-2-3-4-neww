041261aac1be9bb4386abb502828409fe93c857d
KJJK2004 <150164535+KJJK2004@users.noreply.github.com>
2023-12-10 00:53:59 +0330

    Add files via upload

---
+++ pro1.cpp
@@ -0,0 +1,29 @@
+#incloud <iostream>
+using namespace std;
+int main() {
+   flout num1, num2, result;
+   char op;
+   std::cout << "PLEASE ENTER EXPESSION : ";
+   std::cin  >> num1 >> op >> num2;
+   switch  (op){
+   	    case'+'
+   	    	result = num1 + num2;
+   	    	std::cout<<result; 
+   	    	break;
+   	    case '-'
+   	        result = num1 - num2;
+   	        std::cout<<result;
+   	        break;
+   	    case'*'
+   	        result = num1 * num2;
+   	        std::cout<<result;
+   	        break;
+   	    case'/'
+   	        result = num1 / num2;
+   	        std::cout<<result;
+   	        break;
+   	        
+   	        
+   	        
+   	        
+   }
